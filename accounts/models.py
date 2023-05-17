ea2c6e44c044edaf3934b93fe2881a134d35189a
Juantrevi <juantrevi70@gmail.com>
2023-05-18 00:29:24 +0100

    Initial commit

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, User
+
+
+class UserA(User, PermissionsMixin):
+
+    def __str__(self):
+        return '@{}'.format(self.username)
